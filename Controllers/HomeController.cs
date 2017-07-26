db1e0cb6a74caa2484a7229a19a826617805cbc6
Christopher Helgeson <ChristopherHelgeson@hotmail.com>
2017-07-26 06:49:46 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Welcome, new customer! Please register to use our site.";
+
+            return View();
+        }
+
+        public ActionResult Login(Register n)
+        {
+            ViewBag.Message = "Welcome, " + n.FirstName + ".";
+
+            return View(n);
+        }
+
+        public ActionResult Summary(Register r)
+        {
+            ViewBag.Message = "Your email is " + r.Email;
+            return View(r);
+        }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace CoffeeShop.Models
+{
+    public class Register
+    {
+        [Required]
+        [Key]
+        public string FirstName { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
